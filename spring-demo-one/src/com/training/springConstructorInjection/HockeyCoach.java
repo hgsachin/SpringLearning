280c9123064b59b91d8ce43e2274ae6448a9eed9
Sachin HG <hgsachin@gmail.com>
2019-05-28 21:57:58 +0530

    Setter Injection example

--- spring-demo-one/src/com/training/springConstructorInjection/HockeyCoach.java
+++ spring-demo-one/src/com/training/springConstructorInjection/HockeyCoach.java
@@ -7,11 +7,21 @@ import org.springframework.stereotype.Component;
 public class HockeyCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	public HockeyCoach() {
+		System.out.println("HockeyCoach: Inside Constructor...");
+	}
 
 	@Autowired
-	public HockeyCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("HockeyCoach: Inside Setter method...");
+		this.fortuneService = fortuneService;
 	}
+
+	/*
+	 * @Autowired public HockeyCoach(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService; }
+	 */
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
